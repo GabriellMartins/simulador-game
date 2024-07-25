79b15d4fb0a2526e8105c84b54833cf2adb96819
GabrielMartins <102445948+GabriellMartins@users.noreply.github.com>
2024-07-24 21:08:29 -0300

    comit

--- src/main/java/com/br/minecraft/room/RoomManager.java
+++ src/main/java/com/br/minecraft/room/RoomManager.java
@@ -1,6 +1,11 @@
 package com.br.minecraft.room;
 
 import com.br.minecraft.SimuladorGame;
+import com.br.minecraft.room.world.VoidGenerator;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.WorldCreator;
+import org.bukkit.WorldType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,7 +22,45 @@ public class RoomManager {
 
     public void LoadRooms(int count) {
         for (int i = 0; i < count; i++) {
-//            GameRoom room = ();
+            GameRoom room = createRoom("Room_" + (i + 1));
+            if (room != null) {
+                rooms.add(room);
+                Bukkit.getLogger().info("Room " + room.getName()+ " loaded.");
+
+            }
         }
     }
+
+    public void unloadRooms() {
+        for (GameRoom room : rooms) {
+            room.unload();
+            Bukkit.getLogger().info("Room " + room.getName() + " unloaded");
+
+        }
+
+        rooms.clear();
+    }
+
+    private GameRoom getRoom(String name) {
+        for (GameRoom room : rooms) {
+            if (room.getName().equals(name)) {
+                return room;
+            }
+        }
+        return null;
+    }
+
+    public List<GameRoom> getRooms() {
+        return rooms;
+    }
+
+    private GameRoom createRoom(String name) {
+        WorldCreator wc = new WorldCreator(name);
+        wc.environment(World.Environment.NORMAL);
+        wc.type(WorldType.FLAT);
+        wc.generator(new VoidGenerator());
+        World world = wc.createWorld();
+        return new GameRoom(name, world);
+    }
+
 }
